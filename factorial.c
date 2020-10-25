243b7875d499e62900f62f2bde8f698c49b4aa94
ashiya135 <73420806+ashiya135@users.noreply.github.com>
2020-10-25 13:49:52 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,21 +1,19 @@
 //Using this code you can calculate the factorial of any given number
 
-#include <stdio.h>
+
+#include<stdio.h>
+long int multiplyNumbers(int n);
 int main() {
-    int n, i;
-    unsigned long long fact = 1;
-    printf("Enter an integer: ");
-    scanf("%d", &n);
-
-    // shows error if the user enters a negative integer
-    if (n < 0)
-        printf("Error! Factorial of a negative number doesn't exist.");
-    else {
-        for (i = 1; i <= n; ++i) {
-            fact *= i;
-        }
-        printf("Factorial of %d = %llu", n, fact);
-    }
-
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    printf("Factorial of %d = %ld", n, multiplyNumbers(n));
     return 0;
+}
+
+long int multiplyNumbers(int n) {
+    if (n>=1)
+        return n*multiplyNumbers(n-1);
+    else
+        return 1;
 }
