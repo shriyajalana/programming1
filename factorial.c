456a1ba90a98bc573835c5bf5f65a57e9cdfdc52
afrozxman <72957058+afrozxman@users.noreply.github.com>
2021-10-07 09:57:20 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,14 +1,10 @@
-//Using this code you can calculate the factorial of any given number
 #include<stdio.h>
 int main()
 {
 	int n,i,fact=1;
-	scanf("%d",&fact);   //Enter a number
+	scanf("%d",&fact);
 	for(i=1;i<=fact;i++)
-	{
-		fact=fact*i;   //storing each i in fact variable
-	}
-	printf("%d",fact);   //This will give you the output of the factorial of the given Number
+		fact=fact*i;
+	printf("%d",fact);
 	return 0;
 }
-//The End 
