77e766e4b0b2a416b00fa484e84a4211ffc96017
amitkumar784 <72292081+amitkumar784@users.noreply.github.com>
2021-10-07 15:07:14 +0530

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -1,9 +1,21 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 int main()
 {
-	int a,b;
-	cin>>a>>b;
-	cout<<a+b;
-	return 0;
+    int n;
+    cin >> n;
+    vector<int> a;
+    while (n > 0)
+    {
+        int k = n % 2;
+        a.push_back(k);
+        n = n / 2;
+    }
+    for (int i = a.size() - 1; i >= 0; i--)
+    {
+        cout << a.at(i) << " ";
+    }
+    cout << endl;
+    return 0;
 }
