c2d16adfd345ada36765067954d834594934dfdb
ashrafietian <56229560+ashrafietian@users.noreply.github.com>
2021-10-07 14:52:10 +0530

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -1,9 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-	int a,b;
-	cin>>a>>b;
-	cout<<a+b;
-	return 0;
+    int n, i;
+    cin >> n;
+    int pro = 1;
+    for (i = 1; i <= n; i++)
+    {
+        pro = pro * i;
+    }
+    cout << pro << endl;
+    return 0;
 }
+
+// int factorial(int n)
+// {
+//     if (n == 1)
+//     {
+//         return 1;
+//     }
+//     return n * factorial(n - 1);
+// }
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     cout << factorial(n) << endl;
+// }
