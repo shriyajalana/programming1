b15ac9f131ceb91b86bdf6091a63a32ecc4f99e2
saziya135 <73420289+saziya135@users.noreply.github.com>
2022-10-07 22:41:55 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,29 +1,66 @@
-#include <iostream>
+// Delete a node at nth position
+
+#include<iostream>
 using namespace std;
-int main()
-{
-    int n, i;
-    cin >> n;
-    int pro = 1;
-    for (i = 1; i <= n; i++)
-    {
-        pro = pro * i;
+struct Node{
+    int data;
+    Node* next;
+};
+Node* head;
+
+void Insert(int data){
+    Node* temp=new Node;
+    temp->data=data;
+    temp->next=NULL;
+    if(head==NULL){
+        head=temp;
+        return;
     }
-    cout << pro << endl;
-    return 0;
+    Node* temp1=head;
+    while(temp1->next!=NULL){
+        temp1=temp1->next;
+    }
+    temp1->next=temp;   
+    
 }
 
-// int factorial(int n)
-// {
-//     if (n == 1)
-//     {
-//         return 1;
-//     }
-//     return n * factorial(n - 1);
-// }
-// int main()
-// {
-//     int n;
-//     cin >> n;
-//     cout << factorial(n) << endl;
-// }
+void Print(){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"  ";
+        temp=temp->next;
+    }
+    cout<<"\n";
+}
+
+void Delete(int p){
+    Node* temp;
+    temp=head;
+    if(p==1){
+        head=temp->next;
+        free(temp);     //delete node
+        return;
+    }
+    Node* temp1=head;
+    for(int i=0;i<p-2;i++){
+        temp1=temp1->next;   //p-1 node
+    }
+    Node* temp2=temp1->next; //p node
+    temp1->next=temp2->next; // connection p-1th node to p+1th node 
+    free(temp2);   //delete node free pth node
+}
+
+int main(){
+    head=NULL;
+    Insert(7);
+    Insert(9);
+    Insert(2);
+    Insert(45);
+    Print();
+    cout<<"Enter position to delete \n";
+    int pos;
+    cin>>pos;
+    Delete(pos);
+    Print();
+    return 0;
+}
