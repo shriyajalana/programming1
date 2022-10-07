94320108db64d1522a3623aba70ae1d365f4a42e
kareem135 <73422236+kareem135@users.noreply.github.com>
2022-10-07 21:52:18 +0530

    Update summation.cpp

--- summation.cpp
+++ summation.cpp
@@ -1,37 +1,80 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+struct Node
+{
+    int data;
+    Node *next;
+};
+Node *head = NULL;
+void insert(int data)
+{
+    Node *temp = new Node;
+    temp->data = data;
+    temp->next = NULL;
+    temp->next = head;
+    head = temp;
+    return;
+}
+
+void print()
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void secondLast()
+{
+    Node *temp = head;
+    temp = temp->next;
+    if (temp->next->next->next->data == 2)
+    {
+        cout << "Yes it is equal to two\n";
+    }
+    cout << temp->data << endl;
+}
+void swap()
+{
+    Node *temp1 = head;
+    Node *temp2 = temp1->next;
+    while (temp1 != NULL && temp2->next != NULL)
+    {
+        int x;
+        x = temp1->data;
+        temp1->data = temp2->data;
+        temp2->data = x;
+        temp1 = temp2->next;
+        temp2 = temp1->next;
+    }
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
 int main()
 {
-    int t;
-    cin>>t;
-    while(t-->0)
-    {
-        int n;
-        cin>>n;
-        int a[n];
-        int i;
-        for(i=0;i<n;i++)
-        {
-            cin>>a[i];
-        }
-        int x,sum=0;
-
-        for(i=0;i<n;i++)
-        {
-            if(i==n-1 && (n!=2))
-            {
-                x=a[i]-a[0];
-                x=abs(x);
-                sum=sum+x;              
-            }
-            if(i<n-1)
-            {
-                x=a[i]-a[i+1];
-                x=abs(x);
-                sum=sum+x;
-            }
-        }
-        cout<<sum<<endl;
-    }
+    insert(4);
+    print();
+    insert(2);
+    print();
+    insert(5);
+    print();
+    insert(8);
+    print();
+    insert(1);
+    print();
+    insert(9);
+    print();
+    // insert(11);
+    // print();
+    swap();
     return 0;
 }
