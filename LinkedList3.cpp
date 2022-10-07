af3c4306c447bdf01211d992110500177e6230d7
saziya135 <73420289+saziya135@users.noreply.github.com>
2022-10-07 22:41:35 +0530

    Update LinkedList3.cpp

--- LinkedList3.cpp
+++ LinkedList3.cpp
@@ -1,33 +1,65 @@
-// 1,2,3,6,4,5,7,6,9
+// Inserting a new node at nth position of the list
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
-int main()
+struct Node
 {
-    int n;
-    cin >> n;
-    int array[n];
-    for (int i = 0; i < n; i++)
+    int data;
+    Node *next;
+};
+Node *head;
+
+void Insert(int data, int n)
+{
+    Node *temp1 = new Node;
+    temp1->data = data;
+    temp1->next = NULL;
+    if (n == 1)
     {
-        cin >> array[i];
+        temp1->next = head;
+        head = temp1;
+        return;
     }
-    vector<int> myVect;
-    int k;
-    cin >> k;
-    for (int i = 0; i < n; i++)
+    Node *temp2 = head;
+    for (int i = 0; i < n - 2; i++)
     {
-        if (array[i] != k)
-        {
-            myVect.push_back(array[i]);
-        }
+        temp2 = temp2->next;
     }
-    cout << "Vector is: ";
-    for (int j = 0; j < myVect.size(); j++)
+    temp1->next = temp2->next;
+    temp2->next = temp1;
+}
+
+void Print()
+{
+    Node *temp = head;
+    cout << "the list is...  ";
+    while (temp != NULL)
     {
-        cout << myVect[j] << ", ";
+        cout << temp->data << "  ";
+        temp = temp->next;
     }
     cout << endl;
 }
 
-// 1,2,3,4,5
+int main()
+{
+    head = NULL;
+    int n, data, num;
+    int k = n;
+    cout << "how many number do you wanted to insert?\n";
+    cin >> num;
+    while (n > 0)
+    {
+        cout << "enter number\n";
+        cin >> data;
+        cout << "at which position\n";
+        cin >> n;
+        Insert(data, n);
+        n--;
+    }
+    Print();
+
+    return 0;
+}
