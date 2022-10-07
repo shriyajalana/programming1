d875b1e3d575506ec7b5dc0d48b0e6246715946e
husnara135 <73411974+husnara135@users.noreply.github.com>
2022-10-07 22:55:39 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,20 +1,65 @@
+// Inserting a new node at nth position of the list
+
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-void print(int n)
+struct Node
 {
-    if (n == 0)
+    int data;
+    Node *next;
+};
+Node *head;
+
+void Insert(int data, int n)
+{
+    Node *temp1 = new Node;
+    temp1->data = data;
+    temp1->next = NULL;
+    if (n == 1)
     {
+        temp1->next = head;
+        head = temp1;
         return;
     }
-    print(n - 1);
-    cout << n << " ";
+    Node *temp2 = head;
+    for (int i = 0; i < n - 2; i++)
+    {
+        temp2 = temp2->next;
+    }
+    temp1->next = temp2->next;
+    temp2->next = temp1;
+}
+
+void Print()
+{
+    Node *temp = head;
+    cout << "the list is...  ";
+    while (temp != NULL)
+    {
+        cout << temp->data << "  ";
+        temp = temp->next;
+    }
+    cout << endl;
 }
 
 int main()
 {
-    int n;
-    cin >> n;
-    print(n);
-    cout << endl;
+    head = NULL;
+    int n, data, num;
+    int k = n;
+    cout << "how many number do you wanted to insert?\n";
+    cin >> num;
+    while (n > 0)
+    {
+        cout << "enter number\n";
+        cin >> data;
+        cout << "at which position\n";
+        cin >> n;
+        Insert(data, n);
+        n--;
+    }
+    Print();
+
     return 0;
 }
