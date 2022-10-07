d65bdb0c09e2133ebeea572e670baf6902616895
husnara135 <73411974+husnara135@users.noreply.github.com>
2022-10-07 22:52:48 +0530

    Update LinkedList1.cpp

--- LinkedList1.cpp
+++ LinkedList1.cpp
@@ -1,49 +1,55 @@
-// insert a node at the beginning of the list
-#include <iostream>
+//reverse a linked list using recurssion method
+#include<iostream>
 using namespace std;
-
-struct Node
-{
-    int data; //creating a node with two variable one is data and another one is pointer
-    Node *next;
+struct Node{
+    int data;
+    Node* next;
 };
-
-Node *Insert(Node *head, int x)
-{
-    Node *temp = new Node; //creating a node
-    temp->data = x;        //storing data in the node
-    temp->next = NULL;     //storing address in the node
-
-    temp->next = head; // linking node with other node         phle jodo
-    head = temp;       // breaking a link and adding with the head   phir todo
-    return head;
+Node* head;
+void Insert(int n){
+    Node* temp=new Node;
+    temp->data=n;
+    temp->next=NULL;
+    if(head==NULL){
+        head=temp;
+        return;
+    }
+    else{
+        Node* temp1=head;
+        while(temp1->next!=NULL){
+            temp1=temp1->next;
+        }
+        temp1->next=temp;
+    }
+}
+void Print(){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"  ";
+        temp=temp->next;
+    }
+    cout<<endl;
 }
 
-void Print(Node *temp)
-{
-    cout << "Here is the list: ";
-    while (temp->next != NULL)
-    {
-        temp = temp->next;
-        cout << temp->data << " -> ";
+void ReversePrint(Node* p){
+    if(p->next==NULL){
+        head=p;
+        return;
     }
-    cout << endl;
-    cout << temp->data << "  ";
-    cout << "\n";
+    ReversePrint(p->next);
+    Node* q=p->next;
+    q->next=p;
+    p->next=NULL;
 }
 
-int main()
-{
-    Node *head = NULL;
-    int i, n, x;
-    cout << "How many number?\n";
-    cin >> n;
-    for (i = 0; i < n; i++)
-    {
-        cout << "Enter Number\n";
-        cin >> x;
-        head = Insert(head, x);
-    }
-    Print(head);
+int main(){
+    head=NULL;
+    Insert(4);
+    Insert(7);
+    Insert(2);
+    Insert(8);
+    Insert(9);
+    ReversePrint(head);
+    Print();
     return 0;
 }
