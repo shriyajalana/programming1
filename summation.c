ec6092a9b8dd15e8d416fcae32b4b9609fd5cd69
kareem135 <73422236+kareem135@users.noreply.github.com>
2022-10-07 21:52:48 +0530

    Update summation.c

--- summation.c
+++ summation.c
@@ -1,8 +1,59 @@
-#include<iostream>
+
+#include <iostream>
 using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    cout<<"Quitient is: "<<n/10<<"Reminder: "<<n%10<<endl;
-    cout<<sizeof(int*)<<"\n"<<sizeof(int)<<"\n"<<sizeof(char)<<"\n"<<sizeof(char*)<<endl;
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+Node *head = NULL;
+
+void insert(int data, int pos)
+{
+    Node *temp = new Node();
+    temp->data = data;
+    temp->next = NULL;
+    if (pos == 1)
+    {
+        temp->next = head;
+        head = temp;
+        return;
+    }
+    Node *temp1 = head;
+
+    for (int i = 0; i < pos - 2; i++)
+    {
+        temp1 = temp1->next;
+    }
+
+    temp->next = temp1->next;
+    temp1->next = temp;
+}
+
+void print()
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "  ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    insert(3, 1);
+    print();
+    insert(4, 1);
+    print();
+    insert(5, 3);
+    print();
+    insert(31, 2);
+    print();
+    insert(42, 2);
+    print();
+    return 0;
 }
