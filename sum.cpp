54d04d32c66b9dc328af8cc14f27288880acb00c
Afroz Quraishi <afroziiitian@gmail.com>
2023-10-02 15:18:46 +0530

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -1,21 +1,13 @@
 #include<iostream>
-#include<string>
 using namespace std;
-// bool checkPali(string s){
-//     int l=0;
-//     int h=s.size()-1;
-//     while(l<=h){
-//         if(s[l++]!=s[h--]){
-//             return false;
-//         }
-//     }
-//     return false;
-// }
 int main()
 {
-    string s;
-    cin>>s;
-    cout<<"Before: "<<s<<"After: ";
-    cout<<s.erase(s.begin() + 4);
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        sum+=a[i];
+    }
+    court<<"Sum: "<<sum<<endl;
     return 0;
 }
